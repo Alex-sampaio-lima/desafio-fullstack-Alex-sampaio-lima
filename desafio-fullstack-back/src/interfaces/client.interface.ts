0f348dd28cb4c93c45da127c79f1cc9d41a24c56
Alexsander Sampaio <105488166+Alex-sampaio-lima@users.noreply.github.com>
2024-02-16 15:49:54 -0300

    feat: creating the interface and client types

---
+++ desafio-fullstack-back/src/interfaces/client.interface.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+import { clientCreateSchema, clientLoginSchema, clientReadSchema, clientReturnSchema } from "../schemas/client.schemas";
+import { DeepPartial, Repository } from "typeorm";
+import Client from "../entities/client.entity";
+
+export type ClientCreate = z.infer<typeof clientCreateSchema>
+export type ClientRead = z.infer<typeof clientReadSchema>
+export type ClientReturn = z.infer<typeof clientReturnSchema>
+export type ClientBodyUpdate = Omit<ClientCreate, 'admin'>
+export type ClientUpdate = DeepPartial<ClientBodyUpdate>
+export type ClientLoginCreate = z.infer<typeof clientLoginSchema>
+export type ClientLoginReturn = { token: string }
+
+export type ClientRepo = Repository<Client>
