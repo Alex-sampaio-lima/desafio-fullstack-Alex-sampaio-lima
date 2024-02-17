ee48061ffdc13f9a273b39b2d3f7dd814d69aa60
Alexsander Sampaio <105488166+Alex-sampaio-lima@users.noreply.github.com>
2024-02-17 00:56:45 -0300

    feat: creating the contact route

---
+++ desafio-fullstack-back/src/routes/contact.router.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { createContactController, deleteContactController, readContactController, updateContactController } from "../controllers/contact.controllers";
+import { verifyIdClientExists } from "../middlewares/verifyIdClientExists.middleware";
+import { verifyIdExistsContact } from "../middlewares/verifyIdContactExists.middleware";
+import { verifyIfEmailExist } from "../middlewares/verifyIfEmailExist.middleware";
+import { vefiryIfTelIsValid } from "../middlewares/vefiryIfTelIsValid.middleware";
+import { verifyAdmin, verifyContactPermission, verifyPermission, verifyToken } from "../middlewares/globals.middleware";
+import { validateBody } from "../middlewares/validatedBody.middleware";
+import { contactUpdateSchema } from "../schemas/contact.schemas";
+
+
+export const contactRouter: Router = Router();
+
+contactRouter.get("/", verifyToken, verifyAdmin, readContactController)
+contactRouter.post("/:clientId", vefiryIfTelIsValid, verifyIdClientExists, verifyIfEmailExist, createContactController)
+
+contactRouter.use("/:contactId", verifyIfEmailExist)
+
+contactRouter.patch("/:contactId", verifyToken, verifyIdExistsContact, verifyContactPermission, vefiryIfTelIsValid, updateContactController)
+contactRouter.delete("/:contactId", verifyToken, verifyIdExistsContact, verifyContactPermission, deleteContactController)
