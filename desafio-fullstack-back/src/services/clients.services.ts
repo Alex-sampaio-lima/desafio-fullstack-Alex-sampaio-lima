7dc09b82626ad5a1da081027da02637061757a46
Alexsander Sampaio <105488166+Alex-sampaio-lima@users.noreply.github.com>
2024-02-17 00:58:08 -0300

    feat: creating the client service

---
+++ desafio-fullstack-back/src/services/clients.services.ts
@@ -0,0 +1,32 @@
+import Client from "../entities/client.entity";
+import { clientRepo } from "../repositories";
+import { ClientCreate, ClientRead, ClientReturn, ClientUpdate } from "../interfaces/client.interface";
+import { clientReadSchema, clientReturnSchema, clientUpdateSchema } from "../schemas/client.schemas";
+import Contact from "../entities/contact.entity";
+
+
+export const createClientService = async (data: ClientCreate): Promise<ClientReturn> => {
+    const newClient: Client = clientRepo.create(data);
+    await clientRepo.save(newClient);
+    return clientReturnSchema.parse(newClient);
+}
+
+export const readClientsService = async (): Promise<ClientRead> => {
+    const clients: ClientRead = await clientRepo.find({ relations: { contact: true } })
+    return clientReadSchema.parse(clients);
+    // return clients;
+}
+
+export const updateClienteService = async (client: Client, data: ClientUpdate): Promise<ClientReturn> => {
+    const clientUpdate: Client = clientRepo.create({ ...client, ...data });
+    await clientRepo.save(clientUpdate);
+    return clientReturnSchema.parse(clientUpdate);
+}
+
+export const deleteClientService = async (clientId: string): Promise<void> => {
+    await clientRepo.delete({ id: clientId })
+}
+
+
+
+
