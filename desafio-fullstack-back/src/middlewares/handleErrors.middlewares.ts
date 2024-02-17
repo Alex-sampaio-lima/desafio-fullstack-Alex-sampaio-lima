c69c0da6fdd4f77a787b7dea3ae39d2e6eb3a500
Alexsander Sampaio <105488166+Alex-sampaio-lima@users.noreply.github.com>
2024-02-17 00:51:59 -0300

    feat: creating HandleErrors

---
+++ desafio-fullstack-back/src/middlewares/handleErrors.middlewares.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Response, Request } from "express";
+import AppError from "../errors/AppError.error";
+import { z } from "zod";
+import { JsonWebTokenError } from "jsonwebtoken";
+
+export const handlErrors = (err: unknown, req: Request, res: Response, next: NextFunction): Response => {
+    if (err instanceof AppError) {
+        throw res.status(err.status).json({ message: err.message });
+    }
+
+    if (err instanceof z.ZodError) {
+        return res.status(400).json(err.flatten().fieldErrors)
+    }
+
+    if (err instanceof JsonWebTokenError) {
+        return res.status(401).json({ message: err.message })
+    }
+    
+    console.log(err);
+    return res.status(500).json({ message: "Internal Server Error" });
+};
