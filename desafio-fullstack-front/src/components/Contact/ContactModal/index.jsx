81b6b5646493ba8dac1cecb89227ecb809a0d5a7
Alexsander Sampaio <105488166+Alex-sampaio-lima@users.noreply.github.com>
2024-02-21 15:40:03 -0300

    feat: refactoring logic and removing unused stuff

--- desafio-fullstack-front/src/components/Contact/ContactModal/index.jsx
+++ desafio-fullstack-front/src/components/Contact/ContactModal/index.jsx
@@ -4,40 +4,39 @@ import { Input } from "../../forms/Input"
 import { ContactContext } from "../../../providers/contact.provider"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import {contactEditSchema} from "../../../pages/DashBoardPage/contactEdit.Schema"
+import { contactEditSchema } from "../../../pages/DashBoardPage/contactEdit.Schema"
 
-export const ContactModal = ({ children, setIsOpen }) => {
+export const ContactModal = ({ children, setIsOpen, contact }) => {
 
     const { editContact } = useContext(ContactContext)
 
-    const { register, handleSubmit, formState: { errors } } = useForm({
+    const { register, handleSubmit, getValues, formState: { errors } } = useForm({
         resolver: zodResolver(contactEditSchema)
     })
-    const submit = (formData) => {
-        editContact(formData)
-    }
+
     return (
         <div className={`${style.modalOverlay}`} role="dialog">
             <div className={`${style.modalBox}`}>
-                <button className={`${style.closeButton} btn-tertiary`} onClick={() => setIsOpen(false)}>Fechar</button>
                 {children}
-                <form onSubmit={handleSubmit(submit)}>
-
+                <button className={`${style.closeButton} btn-tertiary close`} onClick={() => setIsOpen(false)}>X</button>
+                <form className={`${style.containerFormUpdate}`} >
                     <Input
                         placeholder="Email"
+                        labelClass=""
                         type="email"
-                        className="btn-tertiary login"
+                        className="btn-tertiary login headline"
                         error={errors.email}
                         {...register("email")}
                     />
                     <Input
                         placeholder="Telefone"
                         type="tel"
-                        className="btn-tertiary login"
+                        className="btn-tertiary login headline"
                         error={errors.tel}
                         {...register("tel")}
-                    /> 
-                    <button type="submit">Atualizar</button>
+                    />
+                    <button className="btn-tertiary edit" title="Editar" type="button" onClick={() => editContact(contact.id, getValues(["email", "tel"]))}>Editar</button>
+                    {console.log(getValues(["email", "tel"]))}
                 </form>
             </div>
         </div>
