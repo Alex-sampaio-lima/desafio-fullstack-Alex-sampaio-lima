c825d0f10f967868486ca3b233473d93b7c3f5c5
Alexsander Sampaio <105488166+Alex-sampaio-lima@users.noreply.github.com>
2024-02-21 15:39:54 -0300

    feat: refactoring logic and removing unused stuff

--- desafio-fullstack-front/src/components/Contact/index.jsx
+++ desafio-fullstack-front/src/components/Contact/index.jsx
@@ -1,30 +1,29 @@
 import { useContext, useState } from "react"
 import { ContactContext } from "../../providers/contact.provider"
-import { ClientContext } from "../../providers/client.provider"
 import style from "./style.module.scss"
 import { ContactModal } from "./ContactModal"
 
 
-export const Contact = () => {
 
-    const { contactList, deleteContact, editContact } = useContext(ContactContext)
-    const { client } = useContext(ClientContext)
+export const Contact = ({ contacts }) => {
+
+    const { deleteContact } = useContext(ContactContext)
     const [isOpen, setIsOpen] = useState(false)
+
 
     return (
         <div className={`${style.containerList} marginInputBottom`}>
             <ul className={`${style.containerUl}`}>
-                {contactList.map(contact => (
+                {contacts.map((contact) => (
                     <li className={`${style.containerLi}`} key={contact.id}>
                         <div className={`${style.containerContactInfo}`}>
-                            <h3 className="headline grey">Email:{contact.email}</h3>
-                            <h3 className="headline grey"> Tel:{contact.tel}</h3>
+                            <h3 className="headline black">Email:{contact.email}</h3>
+                            <h3 className="headline black"> Tel:{contact.tel}</h3>
                         </div>
                         <div className={`${style.containerButtons}`}>
                             <button className="btn-tertiary" title="Remover" onClick={() => deleteContact(contact.id)}>Excluir</button>
-                            <button className="btn-tertiary" title="Editar" onClick={() => editContact(contact.id)}>Editar</button>
-                            <button className="btn-tertiary"  onClick={() => setIsOpen(true)}>Editar</button>
-                            {isOpen ? <ContactModal setIsOpen={setIsOpen}></ContactModal> : null}
+                            <button className="btn-tertiary" onClick={() => setIsOpen(true)}>Editar</button>
+                            {isOpen ? <ContactModal setIsOpen={setIsOpen} contact={contact} ></ContactModal> : null}
                         </div>
                     </li>
                 ))}
