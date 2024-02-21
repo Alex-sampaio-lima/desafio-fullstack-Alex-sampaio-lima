2c6bc2d8bdd1db036e5da839a0093c42f9c68191
Alexsander Sampaio <105488166+Alex-sampaio-lima@users.noreply.github.com>
2024-02-21 19:11:25 -0300

    feat: removing commented functions and console logs

--- desafio-fullstack-front/src/providers/client.provider.jsx
+++ desafio-fullstack-front/src/providers/client.provider.jsx
@@ -13,41 +13,10 @@ export const ClientProvider = ({ children }) => {
     const navigate = useNavigate();
     const token = localStorage.getItem("@TOKEN");
 
-
-    // const clientRegister = async (formData) => {
-    //     try {
-    //         setLoading(true);
-    //         const { data } = await api.post("/client", formData)
-    //         setClient(data)
-    //         console.log("Reistro Realizado com sucesso!")
-    //     } catch (error) {
-    //         console.log(error)
-    //         if (error.response?.data == "Name already exists, try another one") {
-    //             alert("Usuario já cadastrado!")
-    //         }
-    //         console.log("Erro ao realizar o Registro")
-    //     } finally {
-    //         setLoading(false)
-    //     }
-    // }
-
-    // const clientLogin = async (formData) => {
-    //     try {
-    //         const { data } = await api.post("/login", formData);
-    //         localStorage.setItem("@TOKEN", data.token);
-    //         setClient(data.user);
-    //         console.log("Login Realizado com Sucesso!")
-    //         // navigate("/dashboard")
-    //     } catch (error) {
-    //         console.log(error.message)
-    //     }
-    // }
-
     const getClient = async () => {
         try {
             const { data } = await api.get("/client", { headers: { Authorization: `Bearer ${token}` } })
             setClient(data[0]);
-            // console.log("Data =", data)
         } catch (error) {
             if (error.response?.data.message === "insufficient permissions") {
                 toast.error("Você não tem permissão")
@@ -66,7 +35,6 @@ export const ClientProvider = ({ children }) => {
     useEffect(() => {
         getClient()
     }, [])
-
 
     return (
         <ClientContext.Provider value={{ client, getClient, setClient, clientLogout }}>
