468048c5a59588311ab2638e4c363b08fcbc4593
Alexsander Sampaio <105488166+Alex-sampaio-lima@users.noreply.github.com>
2024-02-21 19:10:55 -0300

    feat: sharing the contactList by props for the Contact Component

--- desafio-fullstack-front/src/pages/DashBoardPage/index.jsx
+++ desafio-fullstack-front/src/pages/DashBoardPage/index.jsx
@@ -4,17 +4,18 @@ import { Header } from "../../components/Header"
 import { Footer } from "../../components/Footer"
 import { ContactForm } from "../../components/ContactForm"
 import { Contact } from "../../components/Contact"
+import { ContactContext } from "../../providers/contact.provider"
 
 export const DashBoardPage = ({ }) => {
 
-    const { client, getClient, clientLogout } = useContext(ClientContext)
+    const { contactList } = useContext(ContactContext)
 
 
     return (
         <main>
             <Header title="Contatos de Clientes" />
             <ContactForm />
-            <Contact />
+            <Contact contacts={contactList}/>
             <Footer />
         </main>
     )
