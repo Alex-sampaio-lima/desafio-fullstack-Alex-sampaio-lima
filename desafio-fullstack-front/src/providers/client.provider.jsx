74da1dd5349f4a08826f781d0814f9bf338fd23a
Alexsander Sampaio <105488166+Alex-sampaio-lima@users.noreply.github.com>
2024-02-25 21:56:38 -0300

    feat: removing console.logs, passing states to props and fixing functions

--- desafio-fullstack-front/src/providers/client.provider.jsx
+++ desafio-fullstack-front/src/providers/client.provider.jsx
@@ -9,35 +9,67 @@ export const ClientContext = createContext({});
 export const ClientProvider = ({ children }) => {
 
     const [client, setClient] = useState(null);
+    const [clientList, setClientList] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [count, setCount] = useState(false);
     const navigate = useNavigate();
-    const token = localStorage.getItem("@TOKEN");
+
+
 
     const getClient = async () => {
+        const id = localStorage.getItem("@USERID")
+        const admin = localStorage.getItem("@ADMIN")
+        const token = localStorage.getItem("@TOKEN");
         try {
-            const { data } = await api.get("/client", { headers: { Authorization: `Bearer ${token}` } })
-            setClient(data[0]);
+            setLoading(true)
+            const { data } = await api.get(`/client`, { headers: { Authorization: `Bearer ${token}` } })
         } catch (error) {
-            if (error.response?.data.message === "insufficient permissions") {
-                toast.error("Você não tem permissão")
-            }
             console.error(error)
+            if (error.response?.data.message === "insufficient permissions") {
+                toast.error("Você não tem permissão para listar todos os clientes")
+            }
+        } finally {
+            (setLoading(false))
         }
     }
+    const getClientById = async () => {
+        const id = localStorage.getItem("@USERID")
+        const admin = localStorage.getItem("@ADMIN")
+        const token = localStorage.getItem("@TOKEN");
+        try {
+            setLoading(true)
+            const { data } = await api.get(`/client/${id}`, { headers: { Authorization: `Bearer ${token}` } })
+            setClient(data)
+            if (admin == "false") {
+                setClientList(data[0].contact)
+            }
+        } catch (error) {
+            console.error(error)
+            if (error.response?.data.message === "insufficient permissions") {
+                toast.error("Você não tem permissão para listar todos os clientes")
+            }
+        } finally {
+            (setLoading(false))
+        }
+    }
+
+    useEffect(() => {
+        const admin = localStorage.getItem("@ADMIN")
+        getClientById()
+
+    }, [count])
 
     const clientLogout = () => {
         setClient(null)
         navigate("/")
         localStorage.removeItem("@TOKEN")
         localStorage.removeItem("@USERID")
+        localStorage.removeItem("@SUBID")
+        localStorage.removeItem("@ADMIN")
     }
 
-    useEffect(() => {
-        getClient()
-    }, [])
-
     return (
-        <ClientContext.Provider value={{ client, getClient, setClient, clientLogout }}>
+        <ClientContext.Provider value={{ client, getClient, setClient, clientLogout, loading, setLoading, clientList, getClientById, setClientList, setCount, count }}>
             {children}
         </ClientContext.Provider>
 
