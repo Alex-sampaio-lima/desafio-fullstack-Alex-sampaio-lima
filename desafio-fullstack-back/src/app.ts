2226a31dce6a9d006ec2762354d06bfbf5424764
Alexsander Sampaio <105488166+Alex-sampaio-lima@users.noreply.github.com>
2024-02-25 21:41:18 -0300

    feat: creating the documentation route

--- desafio-fullstack-back/src/app.ts
+++ desafio-fullstack-back/src/app.ts
@@ -2,14 +2,17 @@ import express, { Application, json } from "express";
 import { routes } from "./routes";
 import { handlErrors } from "./middlewares/handleErrors.middlewares";
 import { handleZodErrors } from "./middlewares/handleZodErrors.middleware";
-
 import cors from "cors";
+import swaggerUiExpress from "swagger-ui-express"
+import swaggerDocument from "./swagger.json"
 
 export const app: Application = express();
 
 app.use(json());
 
 app.use(cors());
+
+app.use("/api-documentation", swaggerUiExpress.serve, swaggerUiExpress.setup(swaggerDocument))
 
 app.use("/", routes)
 
