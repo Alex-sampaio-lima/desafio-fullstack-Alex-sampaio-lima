a7abfa5ee0e65f26bb9fb285b41b7f12ae3e74c3
Alexsander Sampaio <105488166+Alex-sampaio-lima@users.noreply.github.com>
2024-02-25 21:43:14 -0300

    feat: creating the controller for the function to list customers by id

--- desafio-fullstack-back/src/controllers/contact.controllers.ts
+++ desafio-fullstack-back/src/controllers/contact.controllers.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { createContactService, deleteContactService, readContactService, updateContactService } from "../services/contact.service";
+import { createContactService, deleteContactService, readContactByIdService, readContactService, updateContactService } from "../services/contact.service";
 import Contact from "../entities/contact.entity";
 import { ContactRead } from "../interfaces/contact.interface";
 
@@ -13,11 +13,16 @@ export const readContactController = async (req: Request, res: Response) => {
     const contacts: ContactRead = await readContactService()
     return res.status(201).json(contacts);
 }
+export const readContactByIdController = async (req: Request, res: Response) => {
+    const contactId  = req.params.contactId
+    const contact: ContactRead = await readContactByIdService(contactId)
+    return res.status(200).json(contact)
+}
 
 export const updateContactController = async (req: Request, res: Response) => {
     const foundContact = req.params.contactId
     const contact = await updateContactService(foundContact, req.body)
-    return res.status(201).json(contact);
+    return res.status(200).json(contact);
 }
 
 export const deleteContactController = async (req: Request, res: Response) => {
